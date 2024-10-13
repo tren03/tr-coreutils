537e5e08fb002e0af206bb4a9c22b84b87c8bab3
vishnu <vishnusethuraman@gmail.com>
2024-10-14 00:28:07 +0530

    added support for stdin

--- trwc/trwc.go
+++ trwc/trwc.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"fmt"
-	"io/fs"
+	"io"
+	"log"
 	"os"
 	"strings"
 )
 
-func byteCount(f fs.FileInfo) int {
-	return int(f.Size())
+func byteCount(data []byte) int {
+	return len(data)
 }
 
 func wordCount(data []byte) int {
@@ -43,9 +44,7 @@ func getBytes() {
 			continue
 		}
 
-		nbytes := byteCount(info)
-
-		fmt.Printf("bytes=%d %s \n", nbytes, item)
+		fmt.Printf("bytes=%d %s \n", info.Size(), item)
 		total_size += int(info.Size())
 	}
 	if len(os.Args) > 3 {
@@ -116,34 +115,7 @@ func getWords() {
 
 }
 
-func main() {
-	if len(os.Args) == 2 {
-		// will do default operation later
-		item := os.Args[1]
-		info, err := os.Stat(item)
-		if err != nil {
-			fmt.Println("invalid file path")
-			return
-		}
-		if info.IsDir() {
-			fmt.Printf("item : %s, Cannot process dir \n", item)
-			return
-		}
-		data, err := os.ReadFile(item)
-		fmt.Println(info.Name())
-		fmt.Println("bytes",byteCount(info))
-		fmt.Println("word",wordCount(data))
-		fmt.Println("line",lineCount(data))
-		return
-	}
-	if len(os.Args) == 1 {
-		fmt.Println("wecome to trwc :)")
-		fmt.Println("-c for bytes")
-		fmt.Println("-l for lines")
-		fmt.Println("-w for words")
-        return
-	}
-
+func execCommand() {
 	command := os.Args[1]
 
 	switch command {
@@ -160,5 +132,83 @@ func main() {
 		fmt.Println("-w for words")
 
 	}
+
+}
+
+func main() {
+
+	// CHECKING STDIN
+	content, err := io.ReadAll(os.Stdin)
+	if err != nil {
+		if err == io.EOF {
+			log.Println("reached end")
+		} else {
+			log.Println("err reading from std", err)
+		}
+	}
+	if len(content) != 0 {
+		if len(os.Args) == 1 {
+			fmt.Println("bytes", byteCount(content))
+			fmt.Println("word", wordCount(content))
+			fmt.Println("line", lineCount(content))
+			return
+		} else if len(os.Args) == 2 {
+			command := os.Args[1]
+
+			switch command {
+			case "-c":
+				fmt.Printf("bytes=%d\n",byteCount(content))
+			case "-l":
+				fmt.Printf("lines=%d\n",lineCount(content))
+            case "-w":
+				fmt.Printf("words=%d\n",wordCount(content))
+			default:
+				fmt.Println("Invalid command")
+				fmt.Println("-c for bytes")
+				fmt.Println("-l for lines")
+				fmt.Println("-w for words")
+
+			}
+		}else{
+            fmt.Println("too many commands")
+            return
+        }
+
+
+		return
+	}
+
+	// FILE OPERATIONS
+	if len(os.Args) == 1 {
+		fmt.Println("wecome to trwc :)")
+		fmt.Println("-c for bytes")
+		fmt.Println("-l for lines")
+		fmt.Println("-w for words")
+		return
+	}
+	if len(os.Args) == 2 {
+		item := os.Args[1]
+		info, err := os.Stat(item)
+		if err != nil {
+			fmt.Println("invalid file path")
+			return
+		}
+		if info.IsDir() {
+			fmt.Printf("item : %s, Cannot process dir \n", item)
+			return
+		}
+		data, err := os.ReadFile(item)
+		fmt.Println(info.Name())
+		fmt.Println("bytes", byteCount(data))
+		fmt.Println("word", wordCount(data))
+		fmt.Println("line", lineCount(data))
+		return
+	}
+
+    if len(os.Args) > 2{
+        fmt.Println("too many commands")
+        return
+    }
+	execCommand()
 
 }
--- trwc/trwc_test.go
+++ trwc/trwc_test.go
@@ -6,17 +6,13 @@ import (
 	"testing"
 )
 
+
 func TestByteCount(t *testing.T) {
 
 	expected_byte := 342190
 	expected_word := 58164
 	expected_line := 7145
 
-	data, err := os.Stat("./test.txt")
-	if err != nil {
-		log.Println("err getting stat of test file")
-		t.Fail()
-	}
 	file_content, err := os.ReadFile("./test.txt")
 
 	if err != nil {
@@ -24,7 +20,7 @@ func TestByteCount(t *testing.T) {
 		t.Fail()
 	}
 
-	if expected_byte != byteCount(data) {
+	if expected_byte != byteCount(file_content) {
 		log.Println("byte count off")
 		t.Fail()
 	}
